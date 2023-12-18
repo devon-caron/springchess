d566ae9abce599be73f1dd091345ebcbf5dfae90
devon-caron <devon.caron.p@gmail.com>
2023-12-18 13:49:09 -0800

    added spring initalizer files

---
+++ src/main/java/com/dpc/springchess/SpringchessApplication.java
@@ -0,0 +1,13 @@
+package com.dpc.springchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpc/springchess/SpringchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpc.springchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
